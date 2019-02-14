46fe1526f5123ca60fa72219e21c2b00116371b8
Raymond Hu <rhu4100@bths.edu>
2019-02-13 21:57:51 -0500

    Initial commit

---
+++ src/com/company/CardTester.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class CardTester
+{
+    public static void main(String[] args)
+    {
+        Card one = new Card("4", "7", 1);
+        Card two = new Card("6", "8",1);
+        Card three = new Card("6", "8", 1);
+        System.out.println(one.toString());
+    }
+}
